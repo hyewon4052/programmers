08854a2592dcf290073e379fa18dc12fd6aca024
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2023-05-15 16:51:03 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -7,11 +7,11 @@ class Solution {
         else if(angle==90){
             answer=2;
         }
+        else if (90 < angle && angle< 180) {
+            answer=3;
+        }
         else if(angle==180) {
             answer=4;
-        }
-        else if (90 < angle || angle< 180) {
-            answer=3;
         }
         return answer;
     }
