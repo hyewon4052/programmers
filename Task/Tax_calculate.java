4a5b63b6fdfda0feca2ffe1357f88323ff1ca05a
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-18 23:26:13 +0900

    tax

--- Task/Tax_calculate.java
+++ Task/Tax_calculate.java
@@ -6,25 +6,24 @@ class Tax_calculate
 	{
 		Scanner sc = new Scanner(System.in);
 		
-		double tax;
+		double tax = 0;
 		long salary;
-		tax = 0;
 		System.out.print("연간 소득 입력 : ");
 		salary = sc.nextLong();
-		if (salary<=20000000)
+		if (salary > 80000000)
 		{
-			tax = salary*0.05;
+			tax = salary*0.4;
 		}
-		else if (salary>20000000 && salary<=40000000)
-		{
-			tax = salary*0.15;
-		}
-		else if (salary>40000000 && salary<=80000000)
+		else if (salary >= 40000000)
 		{
 			tax = salary*0.25;
 		}
+		else if (salary >= 20000000)
+		{
+			tax = salary*0.15;
+		}
 		else {
-			tax = salary*0.4;
+			tax = salary*0.05;
 		}
 		System.out.println("근로 소득에 대한 소득세는" + tax + "입니다.");
 	}
