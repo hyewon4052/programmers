279a6727c8f5b4230688fc1e2f6e88f36937babe
hyewon4052 <s2311@e-mirim.hs.kr>
2023-06-06 00:29:07 +0900

    ForTest3

---
+++ Task/ForTest3.java
@@ -0,0 +1,20 @@
+class ForTest3 
+{
+	public static void main(String[] args) 
+	{
+		int i,j;
+		System.out.println("홀수:");
+		for (i = 1; i <=20 ; i++ )
+		{
+			System.out.println(i);
+			i = i + 1;
+		}
+		System.out.println("");
+		System.out.println("짝수:");
+		for (j = 2; j <=20 ; j++ )
+		{
+			System.out.println(j);
+			j = j + 1;
+		}
+	}
+}
