e9e81fbd06857f857ab609bcb31719a26a86e464
hyewon4052 <s2311@e-mirim.hs.kr>
2023-06-05 23:34:08 +0900

    fortest

---
+++ Task/Testfor1.java
@@ -0,0 +1,12 @@
+class Testfor1 
+{
+	public static void main(String[] args) 
+	{
+		int i, p = 0;
+		for (i = 1; i<=10 ; i++ )
+		{
+			p = p + i;
+		}
+		System.out.println("1부터 10까지의 합은: " + p);
+	}
+}
