d192ea9d4b771d5a65d47a8b5e4899333a9dc6d8
hyewon4052 <s2311@e-mirim.hs.kr>
2023-06-07 09:23:44 +0900

    ForTest6

--- Task/ForTest6.java
+++ Task/ForTest6.java
@@ -4,7 +4,7 @@ class ForTest6
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		int c=0,i,input;
+		int cnt=0,i,input;
 
 		System.out.println("정수 10개를 입력하세요: ");
 		for (i = 0; i < 10; i++)
@@ -12,10 +12,10 @@ class ForTest6
 			input = sc.nextInt();
 			if (input % 3 == 0 || input % 5 == 0)
 			{
-				c++;
+				cnt++;
 			}
 		}
-		System.out.println("3과 5의 배수의 개수: " + c);
+		System.out.println("3과 5의 배수의 개수: " + cnt);
 
 	}
 }
