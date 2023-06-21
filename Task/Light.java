46c338b4069d2948cf0488184f46c665daf8e9c4
hyewon4052 <s2311@e-mirim.hs.kr>
2023-06-22 00:01:09 +0900

    Light

--- Task/Light.java
+++ Task/Light.java
@@ -2,5 +2,17 @@ class Light
 {
 	public static void main(String[] args) 
 	{
+		long light = 300000;
+		for ( int s = 1 ; s <= 60 ; s++ )
+		{
+			for (int m = 1 ; m <= 60 ; m++ )
+			{
+				for (int h = 1; h <= 24; h++ )
+				{
+					light += 300000;
+				}
+			}
+		}
+		System.out.println("빛이 하루 동안 가는 거리 : " + light);
 	}
 }
