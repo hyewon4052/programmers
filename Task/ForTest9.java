30b62c689104ecbb4fad8419f51e9c4cfaeca391
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-11 16:02:17 +0900

    ForTest9

--- Task/ForTest9.java
+++ Task/ForTest9.java
@@ -2,14 +2,12 @@ class ForTest9
 {
 	public static void main(String[] args) 
 	{
-		int num = 1;
 		for (int i = 1 ; i <= 5 ; i++ )
 		{
 			for (int j = 1 ; j <= 10 ; j++ )
 			{
-				System.out.print(num);
+				System.out.print(i);
 			}
-			num += 1;
 			System.out.println();
 		}
 	}
