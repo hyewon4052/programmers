f60f79ac4ab94edfe90046e987d6c674d0d5b3bd
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-11 10:54:11 +0900

    ForTest9

---
+++ Task/ForTest9.java
@@ -0,0 +1,16 @@
+class ForTest9 
+{
+	public static void main(String[] args) 
+	{
+		int num = 1;
+		for (int i = 1 ; i <= 5 ; i++ )
+		{
+			for (int j = 1 ; j <= 10 ; j++ )
+			{
+				System.out.print(num);
+			}
+			num += 1;
+			System.out.println();
+		}
+	}
+}
