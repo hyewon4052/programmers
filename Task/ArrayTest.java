2d9208f97bc85032dac5c0aa57b7e93c1d0b9f4a
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-25 22:51:00 +0900

    ArrayTest

---
+++ Task/ArrayTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ArrayTest
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int[]num = new int[3];
+		for (int i = 0 ; i < num.length ; i++)
+		{
+			System.out.print("수를 입력하시오 : ");
+			int n = sc.nextInt();
+			num[i] = n;
+		}
+		for (int i = 0; i < num.length ; i++)
+		{
+			System.out.println(num[i]);
+		}
+	}
+}
