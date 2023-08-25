cd39b0b31b39559e11d5149c7f56f15fda858d3b
hyewon4052 <s2311@e-mirim.hs.kr>
2023-08-25 23:51:51 +0900

    Dollar

--- Task/Dollar.java
+++ Task/Dollar.java
@@ -8,7 +8,7 @@ class Dollar
 		int won;
 		double dollar;
 
-		System.out.print("원화를  입력하세요(단위:원) : ");
+		System.out.print("원화를 입력하세요(단위:원) : ");
 		won = sc.nextInt();
 		dollar = (double)won/1260;
 		System.out.printf(won + "원은  $%.2f입니다.", dollar);
