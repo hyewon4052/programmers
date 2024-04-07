1ad7fc0c0d217f6a1f13aafdc37620b1cf59280f
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-07 22:59:36 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    if (s.length % 2 == 1) 
+        answer = s.slice(s.length/2,(s.length/2)+1);
+    else
+        answer = s.slice((s.length/2)-1,(s.length/2)+1);
+    return answer;
+}
