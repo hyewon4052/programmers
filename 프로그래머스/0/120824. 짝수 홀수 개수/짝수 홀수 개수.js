fc113c295ce56d945096609ebe385cf318c25f91
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-08 14:25:32 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = [0,0];
+    for (var i of num_list) {
+        if (i % 2 == 0)
+            answer[0]++;
+        else
+            answer[1]++;
+    }
+    return answer;
+}
