ef56e1ea77aee25e9adf866aa1f84e70f85b85d1
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-10 23:27:42 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,11 +1,7 @@
 function solution(s) {
-    var answer = '';
-    s = s.split("");
-    s.sort(function(a,b) {
-           if(a < b) return 1;
-           if(a > b) return -1;
-           if(a === b) return 0;
-    })
-    answer = s.join("");
-    return answer;
+    return s
+        .split("")
+        .sort()
+        .reverse()
+        .join("");
 }
