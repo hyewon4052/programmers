ccc858c54e5357fe910b61c32b98a2899141d143
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-23 03:14:52 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,8 @@
+function solution(n)
+{
+    var answer = 0;
+    var arr = n.toString().split("").map(Number);  
+    for (var i = 0; i< arr.length;i++)
+        answer += arr[i];    
+    return answer;
+}
