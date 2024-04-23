e667cf78bdce3c38f0319e2a327efe2144135420
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-23 19:09:22 +0900

    [level 0] Title: 문자열 돌리기, Time: 43.81 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    str = str.split("")
+    for (var i of str){
+        console.log(i)
+    }
+    
+});
