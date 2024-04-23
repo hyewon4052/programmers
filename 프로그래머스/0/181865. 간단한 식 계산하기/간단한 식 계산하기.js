cf2ece4a2930acf456cce112ac47f30ad059e22c
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-23 19:16:24 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,11 @@
+function solution(binomial) {
+    var answer = 0;
+    binomial = binomial.split(" ");
+    if (binomial[1] == "+") 
+        answer = parseInt(binomial[0]) + parseInt(binomial[2]);
+    else if (binomial[1] == "-")
+        answer = parseInt(binomial[0]) - parseInt(binomial[2]);
+    else if (binomial[1] == "*")
+        answer = parseInt(binomial[0]) * parseInt(binomial[2]);
+    return answer;
+}
