9b09bc568dca630483e2f23738d4881626e86f99
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2024-04-23 12:01:06 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,8 @@
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    for (var i = 0; i< my_string.length; i++) {
+         if(my_string.slice(i) == is_suffix)
+            answer = 1;
+    }
+    return answer;
+}
