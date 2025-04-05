b6a76f827fb4dd4512b4fdfa49812879bddf368e
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2025-04-05 17:16:32 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,12 @@
 function solution(array, commands) {
     var answer = [];
-    
-    for (var i = 0; i < commands.length; i++) {
-        var [iIdx, jIdx, kIdx] = commands[i]; 
-        var slicedArray = array.slice(iIdx - 1, jIdx);
-        var sortedArray = slicedArray.sort((a, b) => a - b); 
-        answer.push(sortedArray[kIdx - 1]);  
+    var length = commands.length
+    for(var i = 0; i < length; i++) {
+        var fst = commands[i][0];
+        var lst = commands[i][1];
+        
+        var str = array.slice(fst-1,lst).sort((a,b)=>a-b);
+        answer.push(str[commands[i][2]-1]);
     }
     
     return answer;
