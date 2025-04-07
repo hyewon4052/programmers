6be356f78676c4c574d86d8ce8bf1b7d4660c96e
hyewon4052 <129272496+hyewon4052@users.noreply.github.com>
2025-04-08 03:11:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.23 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,19 +1,26 @@
 function solution(participant, completion) {
     var answer = '';
-    
-        participant = participant.sort();
-        completion = completion.sort();
+    //     participant = participant.sort();
+    //     completion = completion.sort();
+    // for (var i = 0; i < participant.length; i++){
+    //     // if (completion.includes(participant[i]) ) {
+    //     //     completion.splice(completion.indexOf(participant[i]),1);
+    //     // } 
+    //     // else {
+    //     //     answer = participant[i];
+    //     // }
+    //     if (participant[i] !== completion[i]) {
+    //         answer = participant[i]; 
+    //         break;
+    //     };
+    // }
+    let map = new Map();
     for (var i = 0; i < participant.length; i++){
-        // if (completion.includes(participant[i]) ) {
-        //     completion.splice(completion.indexOf(participant[i]),1);
-        // } 
-        // else {
-        //     answer = participant[i];
-        // }
-        if (participant[i] !== completion[i]) {
-            answer = participant[i]; 
-            break;
-        };
+        map.set(participant[i], map.get(participant[i]) ? map.get(participant[i])+1 : 1)
     }
+    for (var i = 0; i < completion.length; i++) {
+        map.set(completion[i], map.get(completion[i])-1);
+    }
+    answer = [...map].filter(([key, value]) => value > 0)[0][0]
     return answer;
 }
